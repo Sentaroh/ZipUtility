f1168d090251d8f53f47c8f4372bc3a776b6a7de
Sentaroh <gm.developer.fhoshino@gmail.com>
2019-07-17 02:41:29 +0900

    Logを保存するディレクトリーをApp固有ディレクトリーに変更

--- ZipUtility/src/main/java/com/sentaroh/android/ZipUtility/GlobalParameters.java
+++ ZipUtility/src/main/java/com/sentaroh/android/ZipUtility/GlobalParameters.java
@@ -214,7 +214,8 @@ public class GlobalParameters extends CommonGlobalParms {
 		setLogLimitSize(2*1024*1024);
 		setLogMaxFileCount(gp.settingLogMaxFileCount);
 		setLogEnabled(gp.settingLogOption);
-		setLogDirName(gp.settingLogMsgDir);
+        setLogDirName(internalRootDirectory+"/Android/data/com.sentaroh.android.SMBExplorer/files/log/");
+//		setLogDirName(gp.settingLogMsgDir);
 		setLogFileName(gp.settingLogMsgFilename);
 		setApplicationTag(APPLICATION_TAG);
 	}
